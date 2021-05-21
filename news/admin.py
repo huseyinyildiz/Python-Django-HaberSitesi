94bfeca76c47ae2a3484947647185171b92f8572
Hüseyin Yıldız <huseyinyildiz005@gmail.com>
2021-05-22 00:16:34 +0300

    Admin Image Gallery

--- news/admin.py
+++ news/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-from news.models import Category, New
+from news.models import Category, New, Images
 
 
+class NewImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title','status']
     list_filter = ['status']
 class NewAdmin(admin.ModelAdmin):
     list_display = ['title','category','status']
     list_filter = ['status','category']
+    inlines = [NewImageInline]
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title','news','image']
+
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(New,NewAdmin)
+admin.site.register(Images,ImagesAdmin)
--- news/models.py
+++ news/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -27,7 +27,7 @@ class New(models.Model):
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=200)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     detail = models.TextField()
@@ -38,5 +38,11 @@ class New(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+class Images(models.Model):
+    news=models.ForeignKey(New,on_delete=models.CASCADE)
+    title = models.CharField(max_length=50,blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
     def __str__(self):
         return self.title
