21be68fe38a7b6a965c88a6af54b75376c0c9562
Hüseyin Yıldız <huseyinyildiz005@gmail.com>
2021-05-22 00:41:28 +0300

    Show images for admin panel

--- news/admin.py
+++ news/admin.py
@@ -8,14 +8,18 @@ class NewImageInline(admin.TabularInline):
     model = Images
     extra = 5
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title','status']
+    list_display = ['title','image_tag','status']
     list_filter = ['status']
+    radeonly_fields=('image_tag',)
+
 class NewAdmin(admin.ModelAdmin):
-    list_display = ['title','category','status']
+    list_display = ['title','category','image_tag','status']
     list_filter = ['status','category']
     inlines = [NewImageInline]
+    readonly_fields = ('image_tag',)
 class ImagesAdmin(admin.ModelAdmin):
-    list_display = ['title','news','image']
+    list_display = ['title','news','image_tag']
+    readonly_fields = ('image_tag',)
 
 
 admin.site.register(Category,CategoryAdmin)
--- news/models.py
+++ news/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS = (
         ('True', 'Evet'),
@@ -20,6 +23,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description='Image'
 
 
 class New(models.Model):
@@ -40,9 +46,15 @@ class New(models.Model):
 
     def __str__(self):
         return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description='Image'
 class Images(models.Model):
     news=models.ForeignKey(New,on_delete=models.CASCADE)
     title = models.CharField(max_length=50,blank=True)
     image = models.ImageField(blank=True, upload_to='images/')
     def __str__(self):
         return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description='Image'
