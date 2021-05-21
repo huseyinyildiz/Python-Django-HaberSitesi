7cfc0a8cf6f845d17948fb3a101f44c8e9e15c73
Hüseyin Yıldız <huseyinyildiz005@gmail.com>
2021-05-21 17:49:42 +0300

    Category and News relationship

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from news.models import Category
+from news.models import Category, New
+
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title','status']
     list_filter = ['status']
+class NewAdmin(admin.ModelAdmin):
+    list_display = ['title','category','status']
+    list_filter = ['status','category']
 
 admin.site.register(Category,CategoryAdmin)
+admin.site.register(New,NewAdmin)
--- news/models.py
+++ news/models.py
@@ -1,21 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     STATUS = (
-        ('True','Evet'),
-        ('False','Hayır'),
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
     )
     title = models.CharField(max_length=30)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    image = models.ImageField(blank=True,upload_to='images/')
-    status =models.CharField(max_length=10,choices=STATUS)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
 
-    slug=models.SlugField()
-    parent= models.ForeignKey('self',blank=True , null=True , related_name='children', on_delete=models.CASCADE)
-    create_at= models.DateTimeField(auto_now_add=True)
-    update_at= models.DateTimeField(auto_now=True)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class New(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    detail = models.TextField()
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
