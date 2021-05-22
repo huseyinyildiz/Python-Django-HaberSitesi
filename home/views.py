6f44472a4b536c6f8ca86e7646dfa2a377251269
Hüseyin Yıldız <huseyinyildiz005@gmail.com>
2021-05-22 16:46:27 +0300

    references,aboutus,contacus

--- home/views.py
+++ home/views.py
@@ -7,6 +7,24 @@ from home.models import Settings
 
 def index(request):
     setting=Settings.objects.get(pk=1)
-    context={'setting':setting}
+    context={'setting':setting,'page':'home'}
 
     return render(request,'index.html',context)
+
+def aboutus(request):
+    setting=Settings.objects.get(pk=1)
+    context={'setting':setting,'page':'aboutus'}
+
+    return render(request,'aboutus.html',context)
+
+def references(request):
+    setting=Settings.objects.get(pk=1)
+    context={'setting':setting,'page':'references'}
+
+    return render(request,'references.html',context)
+
+def contactus(request):
+    setting=Settings.objects.get(pk=1)
+    context={'setting':setting,'page':'contactus'}
+
+    return render(request,'contactus.html',context)
